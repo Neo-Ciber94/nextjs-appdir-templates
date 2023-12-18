90b422b3cf091fa33725e087162dded2c4d64162
Freddy L <neo-ciber_94@hotmail.com>
2023-12-17 21:39:54 -0400

    Updated styles

--- templates/drizzle-mysql-lucia-auth/src/app/login/page.tsx
+++ templates/drizzle-mysql-lucia-auth/src/app/login/page.tsx
@@ -9,8 +9,16 @@ export default async function LoginPage() {
   }
 
   return (
-    <div>
-      <a href="/api/auth/google/login">Login with Google</a>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-11/12 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white p-8 rounded shadow-md">
+        <h1 className="text-2xl font-bold mb-4">Login</h1>
+        <a
+          href="/api/auth/google/login"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full block text-center"
+        >
+          Login with Google
+        </a>
+      </div>
     </div>
   );
 }
--- templates/drizzle-mysql-lucia-auth/src/app/page.tsx
+++ templates/drizzle-mysql-lucia-auth/src/app/page.tsx
@@ -4,23 +4,37 @@ import { redirect } from "next/navigation";
 
 export default async function HomePage() {
   const session = await getUserSession();
+
   if (!session) {
     redirect("/login");
   }
 
   return (
-    <div>
-      <div className="flex flex-row gap-2 items-center">
-        <Image
-          src={session.user.imageUrl}
-          alt={session.user.username}
-          width={64}
-          height={64}
-        />
-        <h1>Hello {session.user.username}</h1>
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-11/12 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white p-8 rounded shadow-md">
+        <div className="flex flex-row gap-2 items-center">
+          <div className="relative w-16 h-16">
+            <Image
+              src={session.user.imageUrl}
+              alt={session.user.username}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full"
+            />
+          </div>
+          <h1 className="text-xl font-bold">Hello</h1>
+          <h1 className="text-2xl font-bold text-blue-800">
+            {session.user.username}
+          </h1>
+        </div>
 
-      <a href="/api/auth/google/logout">Logout</a>
+        <a
+          href="/api/auth/google/logout"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full block text-center mt-4"
+        >
+          Logout
+        </a>
+      </div>
     </div>
   );
 }
